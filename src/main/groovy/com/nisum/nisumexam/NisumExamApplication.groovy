14506ff5d4124cdfa2b67e7204b7e14f8f49e11c
omar <omarberroteranlkf@gmail.com>
2023-09-22 23:14:56 -0600

    Initial commit

---
+++ src/main/groovy/com/nisum/nisumexam/NisumExamApplication.groovy
@@ -0,0 +1,13 @@
+package com.nisum.nisumexam
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NisumExamApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NisumExamApplication, args)
+	}
+
+}
