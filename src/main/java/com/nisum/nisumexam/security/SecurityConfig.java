ce3b706abea576a8600c557303a5dd554e53d2c2
omar <omarberroteranlkf@gmail.com>
2023-09-24 23:12:18 -0600

    fix access H2 console
    Signed-off-by: omar <omarberroteranlkf@gmail.com>

--- src/main/java/com/nisum/nisumexam/security/SecurityConfig.java
+++ src/main/java/com/nisum/nisumexam/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.nisum.nisumexam.security.filters.JWTRequestTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -53,15 +55,25 @@ public class SecurityConfig {
             .requestMatchers("/swagger-resources/**'", "/swagger-ui/**", "/api/api-docs**", "/bus/v3/api-docs/**", "/v3/api-docs/**").permitAll()
             
             .requestMatchers("/h2-console/**").permitAll()
+            .requestMatchers(PathRequest.toH2Console() ).permitAll()
             
             .requestMatchers("/user/signup").permitAll()
             
             .requestMatchers("/Auth/signin**").permitAll().requestMatchers("/Auth/logout**").permitAll()
             
-            .anyRequest().authenticated()).csrf(AbstractHttpConfigurer::disable).cors(withDefaults()).sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .anyRequest().authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(withDefaults()).sessionManagement( (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     
     http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     
     return http.build();
   }
+  
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers("/h2-console/**");
+  }
+  
+  
 }
